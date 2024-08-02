bfa7c604ec68b536732994d3603feb2e5acbd522
Bgmi1233353 <githubvariya@gmail.com>
2024-08-02 20:34:39 +0530

    Update run_m_py.sh

--- run_m_py.sh
+++ run_m_py.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true; do
     python3 m.py
-    sleep 60  # Sleep for 60 seconds between iterations to reduce CPU usage
+    sleep 0.2  # Sleep for 0.2 seconds between iterations to reduce CPU usage
 done
